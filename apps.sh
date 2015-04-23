119a29329c4ddba64c12994d6996dcf77e34c87d
Thibaud Colas <thibaudcolas@gmail.com>
2015-04-23 14:44:25 +1200

    Move things around

--- apps.sh
+++ apps.sh
@@ -3,47 +3,6 @@
 # include my library helpers for colorized echo and require_brew, etc
 source ./lib.sh
 
-bot "OK, let's roll..."
-
-#####
-# install homebrew
-#####
-
-running "checking homebrew install"
-brew_bin=$(which brew) 2>&1 > /dev/null
-if [[ $? != 0 ]]; then
-	action "installing homebrew"
-    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-    if [[ $? != 0 ]]; then
-    	error "unable to install homebrew, script $0 abort!"
-    	exit -1
-	fi
-fi
-ok
-
-running "checking brew-cask install"
-output=$(brew tap | grep cask)
-if [[ $? != 0 ]]; then
-	action "installing brew-cask"
-	require_brew caskroom/cask/brew-cask
-fi
-ok
-
-# Make sure we’re using the latest Homebrew
-running "updating homebrew"
-brew update
-
-bot "before installing brew packages, we can upgrade any outdated packages."
-read -r -p "run brew upgrade? [y|N] " response
-if [[ $response =~ ^(y|yes|Y) ]];then
-    # Upgrade any already-installed formulae
-    action "upgrade brew packages..."
-    brew upgrade
-    ok "brews updated..."
-else
-    ok "skipped brew package upgrades.";
-fi
-
 ###############################################################################
 #Install GUI tools using Homebrew                                             #
 ###############################################################################
